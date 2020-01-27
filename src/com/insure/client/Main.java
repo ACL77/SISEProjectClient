2ccaf9399ea4ba4b73475e58964d9b3df048810f
andrlourenco <andreandebol@gmail.com>
2020-01-27 18:03:41 +0000

    aula 1 para projeto

--- src/com/insure/client/Main.java
+++ src/com/insure/client/Main.java
@@ -1,7 +1,30 @@
 package com.insure.client;
 
+import com.insure.client.gen.ClaimDataStore;
+import com.insure.client.gen.ClaimDataStoreService;
+
+import javax.xml.ws.BindingProvider;
+
 public class Main {
+
+    private static ClaimDataStore docStorage;
+
     public static void main(String args[]){
-        System.out.println("Project template - client");
+
+        System.out.println("Project template - server");
+
+        String url = "http://localhost:8090/docstorage";
+
+        ClaimDataStoreService dss = new ClaimDataStoreService();
+
+        final ClaimDataStore claim = (ClaimDataStore) dss.getClaimDataStorePort();
+
+        ((BindingProvider) claim).getRequestContext().put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, url);
+
+        System.out.println("Done!");
+
+        int claim_id = docStorage.addClaim("ola");
+
+        System.out.println("Claim id created:"+ claim_id);
     }
 }
